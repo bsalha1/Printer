a3600da3b28857d8a8208c1df42389698604efeb
ahdg <ahdg6@outlook.com>
2021-08-05 19:37:45 +0800

    Fix GD Support & Optimized Code Styles

--- src/main/java/com/reliableplugins/printer/hook/territory/griefdefender/GriefDefenderHook.java
+++ src/main/java/com/reliableplugins/printer/hook/territory/griefdefender/GriefDefenderHook.java
@@ -2,6 +2,8 @@ package com.reliableplugins.printer.hook.territory.griefdefender;
 
 import com.flowpowered.math.vector.Vector3i;
 import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.Subject;
+import com.griefdefender.api.Tristate;
 import com.griefdefender.api.claim.Claim;
 import com.griefdefender.api.claim.TrustTypes;
 import com.reliableplugins.printer.hook.territory.ClaimHook;
@@ -14,22 +16,19 @@ public class GriefDefenderHook implements ClaimHook
     public boolean isClaimFriend(Player owner, Player accessor)
     {
         Location loc = owner.getLocation();
-        Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-        Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
-        if(claim == null)
-        {
-            return false;
-        }
+        final Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        final Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
 
-        return claim.getUserTrusts(TrustTypes.BUILDER).contains(accessor.getUniqueId());
+        return claim.getUserTrusts().contains(accessor.getUniqueId());
     }
 
     @Override
     public boolean isInAClaim(Player player)
     {
         Location loc = player.getLocation();
-        Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-        Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
+        if (GriefDefender.getCore().isEnabled(loc.getWorld().getUID())) return false;
+        final Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        final Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
 
         return !claim.isWilderness();
     }
@@ -38,8 +37,8 @@ public class GriefDefenderHook implements ClaimHook
     public boolean isInOwnClaim(Player player)
     {
         Location loc = player.getLocation();
-        Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-        Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
+        final Vector3i vector = Vector3i.from(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        final Claim claim = GriefDefender.getCore().getClaimManager(loc.getWorld().getUID()).getClaimAt(vector);
 
         return player.getUniqueId().equals(claim.getOwnerUniqueId());
     }
@@ -47,15 +46,18 @@ public class GriefDefenderHook implements ClaimHook
     @Override
     public boolean canBuild(Player player, Location location, boolean allowWilderness)
     {
-        Vector3i vector = Vector3i.from(location.getBlockX(), location.getBlockY(), location.getBlockZ());
-        Claim claim = GriefDefender.getCore().getClaimManager(location.getWorld().getUID()).getClaimAt(vector);
+        final Vector3i vector = Vector3i.from(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        final Claim claim = GriefDefender.getCore().getClaimManager(location.getWorld().getUID()).getClaimAt(vector);
+        final Subject subject = GriefDefender.getCore().getSubject(player.getUniqueId().toString());
+        final Tristate canBuild = GriefDefender.getPermissionManager().getActiveFlagPermissionValue(claim, subject,
+                com.griefdefender.api.permission.flag.Flags.BLOCK_PLACE, player, location.getBlock(), null, TrustTypes.BUILDER, true);
 
         if(claim.isWilderness())
         {
             return allowWilderness;
         }
 
-        if(claim.getUserTrusts(TrustTypes.BUILDER).contains(player.getUniqueId()))
+        if(canBuild == Tristate.TRUE)
         {
             return true;
         }
